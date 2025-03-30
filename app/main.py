d2f94ed300b04aeed8ff807915a6f5f094d3b5e9
Daniel ribeiro <danielr.area@gmail.com>
2025-03-30 19:54:01 -0300

    feat: Ajustar main para melhor descrição do projeto

--- app/main.py
+++ app/main.py
@@ -8,4 +8,4 @@ app.include_router(routes.router, prefix="/produtor-rural", tags=["Produtores"])
 
 @app.get("/")
 def read_root():
-    return {"message": "API com arquitetura limpa funcionando!"}
+    return {"message": "API de produtores rurais!"}
