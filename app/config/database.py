1d512c3530a78e2fda38a8f2c5a9e1d8a407a810
Daniel ribeiro <danielr.area@gmail.com>
2025-04-15 10:53:59 -0300

    Atualizar o database.py

--- app/config/database.py
+++ app/config/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://admin:EUamojesus777@danielestudos.c2vgoq0mwt80.us-east-1.rds.amazonaws.com:3306/produtores_rurais"
+DATABASE_URL = ""
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
